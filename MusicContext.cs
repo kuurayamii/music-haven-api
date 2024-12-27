429b2917dc33e2ecdbcebb504bae9c1dd366ac27
kuurayamii <80481150+kuurayamii@users.noreply.github.com>
2024-12-27 13:27:04 -0300

    feat: Add data seeding to TipoAlbum model

--- MusicContext.cs
+++ MusicContext.cs
@@ -5,10 +5,26 @@ namespace MusicHaven
 {
     public class MusicContext(DbContextOptions options) : DbContext(options)
     {
-        //public DbSet<TipoAlbum> TiposAlbum { get; set; }
-        //public DbSet<Album> Albums { get; set; }
+
+        public DbSet<TipoAlbum> TiposAlbum { get; set; }
+
+        public DbSet<Album> Albums { get; set; }
+
         //public DbSet<Track> Tracks { get; set; }
         //public DbSet<Review> Reviews { get; set; }
 
+        /*
+            Re escribe la funcion de EF Core "OnModelCreating" para agregar datos estaticos
+            al modelo que contiene los Tipos de Album, esto, debido a que son solo datos referenciales
+            estaticos.
+        */
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TipoAlbum>().HasData(
+                new TipoAlbum { TipoAlbumId = 0, NombreTipoAlbum = "Single" },
+                new TipoAlbum { TipoAlbumId = 1, NombreTipoAlbum = "LP" },
+                new TipoAlbum { TipoAlbumId = 2, NombreTipoAlbum = "EP" }
+            );
+        }
     }
 }
