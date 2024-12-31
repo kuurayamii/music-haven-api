8400769b5393aee54ebc1546aa00e30f405b1373
kuurayamii <80481150+kuurayamii@users.noreply.github.com>
2024-12-31 13:30:19 -0300

    feat: Add seed for a default album

--- MusicContext.cs
+++ MusicContext.cs
@@ -25,6 +25,19 @@ namespace MusicHaven
                 new TipoAlbum { TipoAlbumId = 2, NombreTipoAlbum = "LP" },
                 new TipoAlbum { TipoAlbumId = 3, NombreTipoAlbum = "EP" }
             );
+
+            modelBuilder.Entity<Album>().HasData
+            (
+                new Album
+                {
+                    AlbumId = 1,
+                    NombreAlbum = "White Pony",
+                    NombreArtista = "Deftones",
+                    Genero = "Nu Metal",
+                    DescripcionAlbum = "Sample text",
+                    TipoAlbumId = 2
+                }
+            );
         }
     }
 }
