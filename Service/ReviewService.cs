a242f64bfda4aff03a53d3865871e5bd5ae0e05e
kuurayamii <80481150+kuurayamii@users.noreply.github.com>
2025-01-06 17:39:01 -0300

    feat: Add db transacts methods

--- Service/ReviewService.cs
+++ Service/ReviewService.cs
@@ -1,6 +1,38 @@
-﻿namespace MusicHaven.Service
+﻿using MusicHaven.Models;
+
+namespace MusicHaven.Service
 {
-    public class ReviewService
+    public class ReviewService(MusicContext dbContext)
     {
+        private readonly MusicContext context = dbContext;
+
+        public IEnumerable<Review> Get() 
+        {
+            throw new NotImplementedException();
+        }
+
+        // Obtener una review en especifico
+        public async Task<Review> GetReview(int idPostReview)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Postear Review
+        public async Task PostReview(Review review) 
+        {
+            throw new NotImplementedException();
+        }
+
+        // Editar Review
+        public async Task PutReview(int idPostReview, Review review)
+        {
+            throw new NotImplementedException();
+        }
+        
+        // Eliminar Review
+        public async Task DeleteReview(int idPostReview)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
