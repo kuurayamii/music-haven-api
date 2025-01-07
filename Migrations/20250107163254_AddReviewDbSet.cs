7f385e85d77ffb6125997dc3f544076684447f14
kuurayamii <80481150+kuurayamii@users.noreply.github.com>
2025-01-07 17:50:15 -0300

    fix: Add dbSet for review

---
+++ Migrations/20250107163254_AddReviewDbSet.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicHaven.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReviewDbSet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Review_Album_AlbumId",
+                table: "Review");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Review",
+                table: "Review");
+
+            migrationBuilder.RenameTable(
+                name: "Review",
+                newName: "Reviews");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Review_AlbumId",
+                table: "Reviews",
+                newName: "IX_Reviews_AlbumId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Reviews",
+                table: "Reviews",
+                column: "ReviewId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Album_AlbumId",
+                table: "Reviews",
+                column: "AlbumId",
+                principalTable: "Album",
+                principalColumn: "AlbumId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Album_AlbumId",
+                table: "Reviews");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Reviews",
+                table: "Reviews");
+
+            migrationBuilder.RenameTable(
+                name: "Reviews",
+                newName: "Review");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Reviews_AlbumId",
+                table: "Review",
+                newName: "IX_Review_AlbumId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Review",
+                table: "Review",
+                column: "ReviewId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Review_Album_AlbumId",
+                table: "Review",
+                column: "AlbumId",
+                principalTable: "Album",
+                principalColumn: "AlbumId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
