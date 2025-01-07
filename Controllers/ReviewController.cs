a7655ff61ab6b1e7209393255a738f53a216ccf8
kuurayamii <80481150+kuurayamii@users.noreply.github.com>
2025-01-07 12:58:55 -0300

    feat: Add post, get (id) and get methods

--- Controllers/ReviewController.cs
+++ Controllers/ReviewController.cs
@@ -1,12 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MusicHaven.Models;
+using MusicHaven.Service;
 
 namespace MusicHaven.Controllers
 {
-    public class ReviewController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReviewController(IReviewService service) : ControllerBase
     {
-        public IActionResult Index()
+        IReviewService reviewService = service;
+
+        // Obtener todas las reviews
+        [HttpGet]
+        public IActionResult GetReviews()
         {
-            return View('s');
+            return Ok(reviewService.GetReviews());
+        }
+
+        // Obtener una sola review
+        [HttpGet("{id}")]
+        public async Task <IActionResult> GetReview(int id)
+        {
+            var review = await reviewService.GetReview(id);
+
+            if (review == null) return NotFound();
+
+            return Ok(review);
+        }
+
+        // Registrar una opinion / review
+        [HttpPost]
+        public async Task <IActionResult> PostReview(Review review)
+        {
+            await reviewService.PostReview(review);
+            return Ok();
+        }
+
+        // Modificar review
+        [HttpPut("{id}")]
+        public async Task <IActionResult> PutReview(int id, Review review)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Elimnar review
+        [HttpDelete("{id}")]
+        public async Task <IActionResult> DeleteReview(int id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
