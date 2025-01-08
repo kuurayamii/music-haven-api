93ac9877f93f0dbcb85041f03c8b21bdecebd278
kuurayamii <80481150+kuurayamii@users.noreply.github.com>
2025-01-08 17:53:57 -0300

    Add put post and get methods

--- Controllers/TrackController.cs
+++ Controllers/TrackController.cs
@@ -1,12 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MusicHaven.Models;
+using MusicHaven.Service;
 
 namespace MusicHaven.Controllers
 {
-    public class TrackController : Controller
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class TrackController(ITrackService service) : ControllerBase
     {
-        public IActionResult Index()
+        ITrackService trackService = service;
+
+        [HttpGet]
+        IActionResult GetTracks() 
         {
-            return View();
+            return Ok(trackService.GetTracks());
         }
+
+        [HttpGet("{id}")]
+        public async Task <IActionResult> GetTrack(int trackId) 
+        {
+            var track = await trackService.GetTrack(trackId);
+
+            if (track == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(track);
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> PostTrack(Track track) 
+        {
+            await trackService.PostTrack(track);
+            return Ok();
+        }
+
+        //[HttpPut("{id}")]
+        public async Task <IActionResult> PutTrack(int trackId, Track track) 
+        {
+            var trackAEditar = await trackService.PutTrack(trackId, track);
+
+            if (trackAEditar == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(trackAEditar);
+        }
+
+        //[HttpDelete("{id}")]
     }
 }
