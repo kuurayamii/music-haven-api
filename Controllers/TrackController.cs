cb56a0b56ed4e7969138fdcfd54094c4aad139a4
kuurayamii <80481150+kuurayamii@users.noreply.github.com>
2025-01-08 13:29:26 -0300

    feat: Add track controller and service

---
+++ Controllers/TrackController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MusicHaven.Controllers
+{
+    public class TrackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
