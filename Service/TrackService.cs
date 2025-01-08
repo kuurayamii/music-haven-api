9162b5b9e7d1cac91dcfea3080c314a71e0219b8
kuurayamii <80481150+kuurayamii@users.noreply.github.com>
2025-01-08 17:54:03 -0300

    Add put post and get methods

--- Service/TrackService.cs
+++ Service/TrackService.cs
@@ -1,6 +1,70 @@
-﻿namespace MusicHaven.Service
+﻿using Microsoft.EntityFrameworkCore;
+using MusicHaven.Models;
+
+namespace MusicHaven.Service
 {
-    public class TrackService
+    public class TrackService(MusicContext dbContext) : ITrackService
     {
+        MusicContext context = dbContext;
+
+        //Get tracks
+        public IEnumerable<Track> GetTracks()
+        {
+
+            return context.Tracks.Include(track => track.Album).ToList();
+
+        }
+
+        // Get Track (POR ID)
+        public async Task<Track> GetTrack(int idTrack)
+        {
+
+            return await context.Tracks
+                .Include(track => track.Album)
+                .FirstOrDefaultAsync(track => track.TrackId == idTrack);
+
+        }
+
+        // Post track 
+        public async Task PostTrack(Track track)
+        {
+
+            await context.AddAsync(track);
+            await context.SaveChangesAsync();
+
+        }
+
+        // Modificar track/tracks (POR ID Y SU OBJETO EN MEMORIA)
+        public async Task<Track> PutTrack(int idTrack, Track track) 
+        {
+            var trackAEditar = await context.Tracks.FindAsync(idTrack);
+
+            if (trackAEditar == null)
+            {
+                return null;
+            }
+
+            trackAEditar.NumeroTrack = track.NumeroTrack;
+            trackAEditar.NombreTrack = track.NombreTrack;
+            await context.SaveChangesAsync();
+
+            return trackAEditar;
+
+        }
+
+        // Eliminar un track (POR ID)
+        public async Task DeleteTrack(int idTrack, Track track) 
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface ITrackService
+    {
+        IEnumerable<Track> GetTracks();
+        Task<Track> GetTrack(int idTrack);
+        Task PostTrack(Track track);
+        Task<Track> PutTrack(int trackId, Track track); 
+        
     }
 }
